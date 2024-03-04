1d3568fd2c74a8ced838b3bb5bb7f90d58c08bf8
WizedWasTaken <jegerfattig1@gmail.com>
2024-03-05 00:25:58 +0100

    Delete unused files and update package.json scripts

---
+++ client/styleguide.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Default Style Guide',
+  components: 'src/components/**/[A-Z]*.vue',
+  defaultExample: true,
+  sections: [
+    {
+      name: 'First Section',
+      components: 'src/components/**/[A-Z]*.vue'
+    }
+  ],
+  webpackConfig: {},
+  exampleMode: 'expand'
+}
